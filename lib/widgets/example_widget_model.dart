8489b83e854e47e83a154eb74ffadf0223750a11
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-07 19:15:44 +0300

    path_provider

--- lib/widgets/example_widget_model.dart
+++ lib/widgets/example_widget_model.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart' as PathProvider;
 
 class ExampleWidgetModel extends ChangeNotifier {
-  void readFile() async {}
+  void readFile() async {
+    
+  }
 }
 
 class ExampleWidgetModelProvider extends InheritedNotifier {
